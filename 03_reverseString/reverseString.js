447c9fec96e340b872def826bd89a4524ff614be
Rene Delion <rene.leon.delion@gmail.com>
2022-12-24 23:46:36 +0100

    Solve the reverseString problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = Array.from(str);
+    for(let i = 0; i < Math.floor(arr.length / 2); i++) {
+        let tmp = arr[i];
+        arr[i] = arr[arr.length - (1 + i)];
+        arr[arr.length - (1 + i)] = tmp;
+    }
+    return arr.join("")
 };
 
 // Do not edit below this line
