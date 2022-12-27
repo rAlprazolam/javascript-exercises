1a67d4446157abb62176c3f13921b41dafb7db1c
Rene Delion <rene.leon.delion@gmail.com>
2022-12-27 10:08:14 +0100

    Solve sumAll problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //Checks for negative inputs
+    if(a < 0 || b < 0) return "ERROR";
+    //Checks for non number inputs
+    if(typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
 
+    sum = 0;
+    if(a < b) {
+        for(let i = a; i <= b; i++) {
+            sum += i;
+        }
+        console.log(sum)
+        return sum;
+    } 
+    else {
+        for(let i = b; i <= a; i++) {
+            sum += i;
+        }
+        console.log(sum)
+        return sum;
+    }
 };
 
 // Do not edit below this line
