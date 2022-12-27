13c2b5b934d081bad26a7a921dc3759fd1a5d9ec
Rene Delion <rene.leon.delion@gmail.com>
2022-12-27 18:58:31 +0100

    Solve tempConversion problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celius = (fahrenheit - 32) * (5/9);
+  return Math.round(celius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celius) {
+  let fahrenheit = (celius * (9 / 5) + 32);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
